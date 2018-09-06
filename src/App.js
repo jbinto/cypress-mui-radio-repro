63c2d7d16ae119f268fbd69b2858baebf3949590
Jesse Buchanan <hello@jessebuchanan.ca>
2018-09-05 20:16:14 -0400

    example case with material-ui and react-final-form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,79 @@
+import { Form, Field } from 'react-final-form';
 import React, { Component } from 'react';
+import MuiRadio from '@material-ui/core/Radio';
+
 import logo from './logo.svg';
 import './App.css';
 
+// ref: https://codesandbox.io/s/2z5y03y81r
+const Radio = ({
+  input: { checked, value, name, onChange, ...restInput },
+  meta,
+  ...rest
+}) => (
+  <MuiRadio
+    {...rest}
+    name={name}
+    inputProps={restInput}
+    onChange={onChange}
+    checked={!!checked}
+    value={value}
+  />
+)
+
+
+// ref: https://github.com/final-form/react-final-form#conditional-fields
+const Condition = (
+  { when, is, children } // eslint-disable-line
+) => (
+  <Field name={when} subscription={{ value: true }}>
+    {({ input: { value } }) => (value === is ? children : null)}
+  </Field>
+)
+
 class App extends Component {
   render() {
+    // ref: https://github.com/final-form/react-final-form#getting-started
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Form
+        onSubmit={(values) => { console.log(values) }}
+        render={({ handleSubmit, values }) => (
+          <form onSubmit={handleSubmit}>
+            <label>
+              <Field
+                type="radio"
+                component={Radio}
+                name="radioGroup"
+                value="optionOne"
+                id="optionOne"
+              />
+              Option One
+            </label>
+
+            <label>
+              <Field
+                type="radio"
+                component={Radio}
+                name="radioGroup"
+                value="optionTwo"
+                id="optionTwo"
+              />
+              Option Two
+            </label>
+
+            <Condition when="radioGroup" is="two">
+              Only visible when "option two" is selected.
+              <Field type="text" component="input" id="textField" name="textField" />
+            </Condition>
+
+            <button type="submit">Submit</button>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+
+          </form>
+        )}
+      >
+
+      </Form>
     );
   }
 }
